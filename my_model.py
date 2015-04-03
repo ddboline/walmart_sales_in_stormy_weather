4d9c5c2bb3693b3fa4fc8403fcf7540b18a883e5
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-03 17:29:12 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -76,12 +76,12 @@ def prepare_submission_parallel(xtrain, ytrain, xtest, ytest, prefix=''):
 
 def my_model(xtrain, ytrain, xtest, ytest, index=0):
     
-    model = RandomForestRegressor(n_jobs=-1, n_estimators=100)
-    train_model_parallel(model, xtrain, ytrain, index, prefix='rf100')
+    #model = RandomForestRegressor(n_jobs=-1, n_estimators=100)
+    #train_model_parallel(model, xtrain, ytrain, index, prefix='rf100')
     
-#    model = GradientBoostingRegressor(loss='ls', verbose=1, max_depth=7, 
-#                                      n_estimators=200)
-#    train_model_parallel(model, xtrain, ytrain, index, prefix='gbr200_7')
+    model = GradientBoostingRegressor(loss='ls', verbose=1, max_depth=5, 
+                                        n_estimators=100)
+    train_model_parallel(model, xtrain, ytrain, index, prefix='gbr100_5')
 
     
     return
